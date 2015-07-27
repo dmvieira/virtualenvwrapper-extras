ea5267a8314568f5e1fc912d90b0d411da14ea72
Diogo Munaro Vieira <diogo.mvieira@gmail.com>
2015-07-27 14:13:17 -0300

    mkdir for create dir if not exists

--- virtualenvwrapperhack.sh
+++ virtualenvwrapperhack.sh
@@ -5,15 +5,17 @@ source /usr/local/bin/virtualenvwrapper.sh;
 
 function to_dir {
     if [ "$2" != "" ]; then
+        mkdir -p $2;
         cd "$WORKSPACE/$2";
     else
+        mkdir -p $1;
         cd "$WORKSPACE/$1";
     fi
 
 }
 
 function hackon {
-    
+
     workon $1;
     to_dir $1 $2;
 }
@@ -23,7 +25,7 @@ function _hackon {
     local digit;
     COMPREPLY=();
     digit=${COMP_WORDS[COMP_CWORD]};
-    COMPREPLY=($(compgen -W "$(basename $(compgen -d $WORKSPACE/))" -- $digit));
+    COMPREPLY=($(compgen -W "$(basename $(ls $WORKSPACE))" -- $digit));
     return 0
 }
 complete -F _hackon -o dirnames hackon;
