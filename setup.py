2708dd7dc078dfc39d28207036f7b91cf2727954
Diogo Munaro Vieira <diogo.mvieira@gmail.com>
2015-07-27 14:14:21 -0300

    pip and setup.py ok

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(name='virtualenvwrapperhack',
+      version='0.1',
+      description='Python virtualenvwrapper hacks',
+      long_description = open('README.md', 'rt').read(),
+      author='Diogo Munaro Vieira',
+      author_email='diogo.mvieira@gmail.com',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=['virtualenv',
+                        'virtualenvwrapper'],
+      data_files = [ ('/usr/local/bin/', ['virtualenvwrapperhack.sh']) ]
+     )
